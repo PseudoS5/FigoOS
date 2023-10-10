ae0ee76e1abbccd559cfef612bdb99741f18b17a
pseudostudios2452 <pseudo_studios@protonmail.com>
2023-10-10 16:30:00 +0200

    Initial commit

---
+++ FigoOS/Kernel.cs
@@ -0,0 +1,25 @@
+﻿using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Sys = Cosmos.System;
+
+namespace FigoOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static VBECanvas canvas = new VBECanvas(new Mode(1920, 1080, ColorDepth.ColorDepth32));
+
+        protected override void BeforeRun()
+        {
+
+        }
+
+        protected override void Run()
+        {
+            canvas.DrawFilledRectangle(new Pen(Color.DarkGreen), 0, 0, 1920, 1080);
+            canvas.Display();
+        }
+    }
+}
